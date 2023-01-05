13473f919b37ded1920774efd54ca1c18301bfd2
PowerLichen <ms.choe015@gmail.com>
2023-01-05 13:57:54 +0900

    feat: add expired filter to shortener

--- api/shortener/views.py
+++ api/shortener/views.py
@@ -1,3 +1,4 @@
+from django.utils import timezone
 from rest_framework import mixins
 from rest_framework.viewsets import GenericViewSet
 
@@ -12,6 +13,9 @@ class ShortenerViewSet(mixins.CreateModelMixin,
                     GenericViewSet):
     queryset = Shortener.objects.all()
     lookup_field = 'code'
+    
+    def get_queryset(self):
+        return Shortener.objects.filter(expired_dt__gte=timezone.now())
 
     def get_serializer_class(self):
         if self.action == 'create':
